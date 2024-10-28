aab373b1cece3b1ffdb54f3d093978407924d08d
Andreea <andreea.celmare@gmail.com>
2024-10-28 11:39:55 +0100

    feat: compare names

--- src/main/java/org/example/Strings.java
+++ src/main/java/org/example/Strings.java
@@ -1,8 +1,15 @@
 package org.example;
 
 public class Strings {
+    public static void main(String[] args) {
+
+
     String name = "Andreea";
     String name2 = new String("Alina");
     String fullName = name + " " + name2;
+
+    if (name.equals("Andreea")) {
+        System.out.println("Name is Andreea");
     }
+}
 }
