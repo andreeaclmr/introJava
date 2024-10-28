4f7d0229a7d58f286812c3b23d2555033a1b31f5
Andreea <andreea.celmare@gmail.com>
2024-10-28 11:46:33 +0100

    feat: add example conditionals User is Active

--- src/main/java/org/example/ControlStructures.java
+++ src/main/java/org/example/ControlStructures.java
@@ -7,6 +7,10 @@ public class ControlStructures {
         // imprimir el usuario está activo si está activo
         // imprimir el usuario no está activo si no está activo
 
-
+        if (isActive) {
+            System.out.println("El usuario está activo");
+        } else {
+            System.out.println("El usuario no está activo");
+        }
     }
 }
