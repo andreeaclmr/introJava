c53e33c3824df123cb4452201487207e83d4dda9
Andreea <andreea.celmare@gmail.com>
2024-10-28 10:29:03 +0100

    feat: boolean type

---
+++ src/main/java/org/example/BasicTypes.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class BasicTypes {
+    public static void main(String[] args) {
+        // escribir un boolean, lo asignamos, lo mostramos por consola
+        boolean isActive = true;
+        System.out.println("isActive");
+    }
+}
