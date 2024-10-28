54513c326c574bd8d43ebf036561ae79df294fc6
Andreea <andreea.celmare@gmail.com>
2024-10-28 11:44:53 +0100

    feat: add ControlStructures Conditionals

---
+++ src/main/java/org/example/ControlStructures.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ControlStructures {
+    public static void main(String[] args) {
+        boolean isActive = true;
+
+        // imprimir el usuario está activo si está activo
+        // imprimir el usuario no está activo si no está activo
+
+
+    }
+}
