f3ca02ca3a57e1403fd1499985decaa88e91e261
Andreea <andreea.celmare@gmail.com>
2024-10-28 12:24:32 +0100

    feat: create ForLopp function to print numbers from 5 to 10

---
+++ src/main/java/org/example/ForLoop.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        // imprimir por consola los numeros de 5 a 10 usando un for
+
+        for (int i = 5; i <= 10; i++) {
+            System.out.println("i = " + 1);
+        }
+    }
+}
