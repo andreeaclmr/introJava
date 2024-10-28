edc588c40e0346fc742e2440cdf0cf81aaa7de02
Andreea <andreea.celmare@gmail.com>
2024-10-28 12:31:18 +0100

    feat: ForLoop even numbers

--- src/main/java/org/example/ForLoop.java
+++ src/main/java/org/example/ForLoop.java
@@ -2,10 +2,12 @@ package org.example;
 
 public class ForLoop {
     public static void main(String[] args) {
-        // imprimir por consola los numeros de 5 a 10 usando un for
+        // imprimir por consola los numeros pares de 5 a 10 usando un for
 
         for (byte i = 5; i <= 10; i++) {
-            System.out.println(i);
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
