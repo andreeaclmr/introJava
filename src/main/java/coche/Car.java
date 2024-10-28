edb86727f597525760c800e0a5aa0455c8a8ba6b
Andreea <andreea.celmare@gmail.com>
2024-10-28 16:12:04 +0100

    feat: add constructor coche

---
+++ src/main/java/coche/Car.java
@@ -0,0 +1,25 @@
+package coche;
+
+// - **Atributos** (variables de instancia):
+//    - `marca` (String)
+//    - `modelo` (String)
+//    - `año` (int)
+//    - `kilometraje` (double)
+
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+    private double km;
+
+    // constructor
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+
+
+}
