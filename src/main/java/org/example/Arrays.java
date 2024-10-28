997741fe52fb2fae36b33e6742b38777803740c5
Andreea <andreea.celmare@gmail.com>
2024-10-28 12:58:02 +0100

    feat: correct arrays 5 city names

--- src/main/java/org/example/Arrays.java
+++ src/main/java/org/example/Arrays.java
@@ -5,7 +5,9 @@ public class Arrays {
         // definir un array de 5 numeros (lleno)
         // un array de 5 nombres de ciudades (llenos)
 
-        int[] numeros = new int[5];
+        int[] numeros = {1, 2, 3, 4, 5};
         String[] nombres = {"Tokyo", "Beijing", "Paris", "Berlin", "London"};
+        System.out.println(numeros);
+        System.out.println(nombres);
     }
 }
