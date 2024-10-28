5c833f191bb0ae456952458ab2193e9d2a8a8922
Andreea <andreea.celmare@gmail.com>
2024-10-28 12:55:43 +0100

    feat: add Arrays and define 5 city names

---
+++ src/main/java/org/example/Arrays.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Arrays {
+    public static void main(String[] args) {
+        // definir un array de 5 numeros (lleno)
+        // un array de 5 nombres de ciudades (llenos)
+
+        int[] numeros = new int[5];
+        String[] nombres = {"Tokyo", "Beijing", "Paris", "Berlin", "London"};
+    }
+}
