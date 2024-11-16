67373ae04654ad3ffb8ce0a873defa35fe5d4d0f
Andreea <andreea.celmare@gmail.com>
2024-11-16 21:35:40 +0100

    feat: try testing Java syntax

---
+++ src/main/java/org/example/ProgrLogic/Sintaxis.java
@@ -0,0 +1,30 @@
+package org.example.ProgrLogic;
+
+public class Sintaxis {
+    public static void main(String[] args) {
+
+        /*  EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+                */
+
+        // https://www.java.com/en/
+
+        int num = 1;
+
+        final double number = 28.50d;
+
+        String string = "string";
+
+        boolean run = true;
+
+        System.out.println("Hola, Java!");
+    }
+}
+
