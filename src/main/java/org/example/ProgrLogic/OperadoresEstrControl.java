3df14a9c38aa861e8cebd3949e42983bbf81de61
Andreea <andreea.celmare@gmail.com>
2024-11-16 21:42:00 +0100

    feat: create operators example

---
+++ src/main/java/org/example/ProgrLogic/OperadoresEstrControl.java
@@ -0,0 +1,39 @@
+package org.example.ProgrLogic;
+
+public class OperadoresEstrControl {
+
+    public static void main(String[] args) {
+
+
+        /*
+         * EJERCICIO:
+         * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+         *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+         */
+
+        int a = 3, b= 15;
+        int sum = a + b;
+        int substract = a - b;
+        int multiply = a * b;
+        int divide = a / b;
+        int module = a % b;
+        System.out.println("This is the " + module + " result");
+
+
+        /*
+         * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+         *   que representen todos los tipos de estructuras de control que existan
+         *   en tu lenguaje:
+         *   Condicionales, iterativas, excepciones...
+         * - Debes hacer print por consola del resultado de todos los ejemplos.
+        */
+
+        /*
+         * DIFICULTAD EXTRA (opcional):
+         * Crea un programa que imprima por consola todos los números comprendidos
+         * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+         */
+
+
+    }
+}
